8133c004a0616d9259b08dae507bd15b1b378897
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-06-16 12:46:27 +0700

    create migration database user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('roles')->default('Customer');
+            $table->integer('total_reservation');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
