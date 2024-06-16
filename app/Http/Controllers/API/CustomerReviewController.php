931709d7f9cb985d187c02e64c7c007041744eae
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-06-16 14:48:58 +0700

    create api submit review customers

---
+++ app/Http/Controllers/API/CustomerReviewController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\CustomerReview;
+use Exception;
+use Illuminate\Http\Request;
+
+class CustomerReviewController extends Controller
+{
+    //create review
+    public function create_review(Request $request)
+    {
+
+
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'rating' => 'required|integer|min:1|max:5',
+                'user_id' => 'required|exists:users,id',
+                'comment' => 'required|string',
+            ]);
+
+
+            $dataReview = CustomerReview::create([
+                'name' => $request->name,
+                'rating' => $request->rating,
+                'user_id' => $request->user_id,
+                'comment' => $request->comment,
+            ]);
+
+
+            return ResponseFormatter::success(
+                [
+                    "data" => $dataReview
+                ],
+                'Success Submit Review'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error(
+                [
+                    "message" => "Something went wrong",
+                    "error" => $error
+                ],
+                "Create Review Failed",
+                500
+            );
+        }
+    }
+}
