19304618e8de542144bd5b2b345264c92c880fa3
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-06-16 13:05:15 +0700

    create migration database reservation and branch salon

---
+++ database/migrations/2024_06_16_055146_create_branch_salons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('branch_salons', function (Blueprint $table) {
+            $table->id();
+            $table->string("branch_name");
+            $table->text("branch_address");
+            $table->time('opening_time');
+            $table->time('closing_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('branch_salons');
+    }
+};
