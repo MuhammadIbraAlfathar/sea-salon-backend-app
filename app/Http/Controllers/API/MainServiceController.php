021e6422f9efc3cafd19419238455554a9be2f28
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-06-17 11:00:02 +0700

    crete api for create new services

---
+++ app/Http/Controllers/API/MainServiceController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\MainServices;
+use Exception;
+use Illuminate\Http\Request;
+
+class MainServiceController extends Controller
+{
+    //create new service
+    public function createService(Request $request)
+    {
+        try {
+            $request->validate([
+                'services_name' => 'required|string',
+                'duration' => 'required',
+            ]);
+
+
+            $dataService = MainServices::create([
+                'services_name' => $request->services_name,
+                'duration' => $request->duration,
+                'description' => $request->description,
+            ]);
+
+            return ResponseFormatter::success(
+                [
+                    "data" => $dataService
+                ],
+                'Success Create Service'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error(
+                [
+                    "message" => "Something went wrong",
+                    "error" => $error
+                ],
+                "Create Review Failed",
+                500
+            );
+        }
+    }
+}
--- app/Models/MainServices.php
+++ app/Models/MainServices.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class MainServices extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'services_name', 'duration', 'description'
+    ];
 }
